da0c1015d5e3e73eac5a356b8547ecc37d75307e
ferehibrahimli123@outlook.com <ferehibrahimli123@outlook.com>
2022-01-11 12:08:51 +0400

    firstm

---
+++ amount/amount/Models/CashRegister.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace amount.Models
+{
+    class CashRegister
+    {
+        public Currency Currency { get; set; }
+        public PaymentType PaymentType { get; set; }
+        public double TotalAmount { get; set; }
+        public static int TotalSalesCount { get; set; }
+        public CashRegister(double totalamount,Currency currency)
+        {
+            TotalAmount = (int)totalamount;
+            Currency = currency;
+        }
+        public void Addnewsale(Currency currency1,double amount)
+        {
+            if (Currency==currency1)
+            {
+                TotalAmount += amount;
+            }
+            else
+            {
+                switch (currency1)
+                {
+                    case Currency.AZN:
+                        amount /= 1.7;
+                        break;
+                    case Currency.USD:
+                        amount *= 1.7;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            TotalAmount += amount;
+        }
+        public void RemoveSale(Currency currency2, double amount)
+        {
+            if (Currency == currency2)
+            {
+                TotalAmount -= amount;
+            }
+            else
+            {
+                switch (currency2)
+                {
+                    case Currency.AZN:
+                        amount /= 1.7;
+                        break;
+                    case Currency.USD:
+                        amount *= 1.7;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            TotalAmount -= amount;
+        }
+        public override string ToString()
+        {
+            return $"{TotalAmount}   {PaymentType}   {Currency}";
+        }
+    }
+}
---
+++ amount/amount/Program.cs
@@ -0,0 +1,14 @@
+﻿using amount.Models;
+using System;
+
+namespace amount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CashRegister cashRegister = new CashRegister(100,Currency.AZN);
+            cashRegister.Addnewsale(110, Currency.USD);
+        }
+    }
+}
